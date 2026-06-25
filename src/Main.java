e98517c9e23467435c39228ba7ea0dfdc6262476
icarodredd <icarog854@gmail.com>
2026-06-25 19:45:30 -0300

    feat: deposit & withdrawal logic

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void clearScreen() {
+    static void clearScreen() {
         try {
             String os = System.getProperty("os.name");
             if (os.contains("Windows")) new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
@@ -13,7 +13,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    static void main(String[] args) throws InterruptedException {
         double balance = 0.00;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bem vindo ao Bento Bank.\n");
@@ -25,10 +25,39 @@ public class Main {
 
             if (input.equals("1")) {
                 TimeUnit.SECONDS.sleep(2);
+                clearScreen();
                 System.out.printf("Saldo disponível: R$ %.2f\n\n", balance);
-                TimeUnit.SECONDS.sleep(2);
+                TimeUnit.SECONDS.sleep(4);
                 clearScreen();
 
+            }
+
+            if (input.equals("2")) {
+                TimeUnit.SECONDS.sleep(2);
+                clearScreen();
+                System.out.println("Qual valor deseja depositar?");
+                double amount = scanner.nextDouble();
+                balance += amount;
+                clearScreen();
+
+                System.out.printf("Seu novo saldo é de R$ %.2f\n\n", balance);
+                TimeUnit.SECONDS.sleep(4);
+
+                clearScreen();
+            }
+
+            if (input.equals("3")) {
+                TimeUnit.SECONDS.sleep(2);
+                clearScreen();
+                System.out.println("Qual valor deseja sacar?");
+                double amount = scanner.nextDouble();
+                balance -= amount;
+                clearScreen();
+
+                System.out.printf("Seu novo saldo é de R$ %.2f\n\n", balance);
+                TimeUnit.SECONDS.sleep(4);
+
+                clearScreen();
             }
 
             if (input.equals("4")) {
@@ -36,6 +65,8 @@ public class Main {
                 break;
             }
         }
+
+        scanner.close();
     }
 
 
